c9f9869e9b627e10259036190b35eda484f2fd69
Inseminador <146298402+Inseminador@users.noreply.github.com>
2023-12-02 23:48:50 -0300

    Update BancoDeDadosLoja.java

--- BancoDeDadosLoja.java
+++ BancoDeDadosLoja.java
@@ -4,16 +4,19 @@ public class BancoDeDadosLoja {
 
     ArrayList<ItemDaLoja> InventarioDaLoja = new ArrayList<>();
 
+    ItemDaLoja granadaM67 = new ItemDaLoja();
+    ItemDaLoja semtex = new ItemDaLoja();
+
     BancoDeDadosLoja(){
-        ItemDaLoja granadaM67 = new ItemDaLoja();
+
         granadaM67.nome = "Granada M-67";
         granadaM67.valor = 925.99;
-        granadaM67.id = 85;
         InventarioDaLoja.add(granadaM67);
-        ItemDaLoja misselBalisticoPadrao = new ItemDaLoja();
-        misselBalisticoPadrao.nome = "Míssel Balístico R-7-1";
-        misselBalisticoPadrao.valor = 1825.99;
-        misselBalisticoPadrao.id = 93;
-        InventarioDaLoja.add(misselBalisticoPadrao);
+        granadaM67.id = 67;
+
+        semtex.nome = "Semtex";
+        semtex.valor = 1825.99;
+        InventarioDaLoja.add(semtex);
+        semtex.id = 76;
     }
 }
